c33bf64164f987c0c3d1b882bfba39c97ecb5331
Reid Miller <r24mille@uwaterloo.ca>
2014-02-13 18:25:04 -0500

    Added unit test for FuelTypeMetadata and found an error in switch statement. I'd forgotten breaks in each case.

--- src/main/java/name/reidmiller/timeofemissions/model/FuelTypeMetadata.java
+++ src/main/java/name/reidmiller/timeofemissions/model/FuelTypeMetadata.java
@@ -2,7 +2,12 @@ package name.reidmiller.timeofemissions.model;
 
 import name.reidmiller.iesoreports.client.GeneratorOutputCapabilityClient.FuelType;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class FuelTypeMetadata {
+	private Logger logger = LogManager.getLogger(this.getClass());
+
 	public static final double HYDROELECTRIC_GRAMS_CO2_PER_KWHE = 4;
 	public static final double WIND_GRAMS_CO2_PER_KWHE = 12;
 	public static final double NUCLEAR_GRAMS_CO2_PER_KWHE = 16;
@@ -12,7 +17,7 @@ public class FuelTypeMetadata {
 	public static final double SOLAR_PV_GRAMS_CO2_PER_KWHE = 46;
 	public static final double NATURAL_GAS_GRAMS_CO2_PER_KWHE = 469;
 	public static final double COAL_GRAMS_CO2_PER_KWHE = 1001;
-	
+
 	public static final String HYDROELECTRIC_GRAPH_COLOR = "#204C79";
 	public static final String WIND_GRAPH_COLOR = "#79D24C";
 	public static final String NUCLEAR_GRAPH_COLOR = "#F9A848";
@@ -28,45 +33,67 @@ public class FuelTypeMetadata {
 		case COAL:
 			this.gramsCarbonDioxidePerKWHe = COAL_GRAMS_CO2_PER_KWHE;
 			this.graphColor = COAL_GRAPH_COLOR;
+			break;
 		case HYDRO:
 			this.gramsCarbonDioxidePerKWHe = HYDROELECTRIC_GRAMS_CO2_PER_KWHE;
 			this.graphColor = HYDROELECTRIC_GRAPH_COLOR;
+			break;
 		case GAS:
 			this.gramsCarbonDioxidePerKWHe = NATURAL_GAS_GRAMS_CO2_PER_KWHE;
 			this.graphColor = NATURAL_GAS_GRAPH_COLOR;
+			break;
 		case NUCLEAR:
 			this.gramsCarbonDioxidePerKWHe = NUCLEAR_GRAMS_CO2_PER_KWHE;
 			this.graphColor = NUCLEAR_GRAPH_COLOR;
+			break;
 		case OTHER:
 			this.gramsCarbonDioxidePerKWHe = BIOMASS_GRAMS_CO2_PER_KWHE;
 			this.graphColor = BIOMASS_GRAPH_COLOR;
+			break;
 		case WIND:
 			this.gramsCarbonDioxidePerKWHe = WIND_GRAMS_CO2_PER_KWHE;
 			this.graphColor = WIND_GRAPH_COLOR;
+			break;
+		default:
+			logger.warn("default case statement reached, which should not happen. fuelType variable is "
+					+ fuelType);
+			break;
 		}
 	}
-	
+
 	public String getGraphColor() {
 		return graphColor;
 	}
 
-	public double getGramsCarbonDioxidePerKWHe(double kilowattHours) {
-		double ratio = this.gramsCarbonDioxidePerKWHe;
-		return ratio * kilowattHours;
+	public double getGramsCarbonDioxidePerKWHe() {
+		return this.gramsCarbonDioxidePerKWHe;
 	}
-	
-	public double getKilogramsCarbonDioxidePerKWHe(double kilowattHours) {
-		double ratio = this.gramsCarbonDioxidePerKWHe / 1000;
-		return ratio * kilowattHours;
+
+	public double getGramsCarbonDioxideForKWHeValue(double kilowattHours) {
+		return (this.getGramsCarbonDioxidePerKWHe() * kilowattHours);
 	}
-	
-	public double getKilogramsCarbonDioxidePerMWHe(double megawattHours) {
-		double ratio = this.gramsCarbonDioxidePerKWHe;
-		return ratio * megawattHours;
+
+	public double getKilogramsCarbonDioxidePerKWHe() {
+		return (this.gramsCarbonDioxidePerKWHe / 1000);
 	}
-	
-	public double getMetricTonnesCarbonDioxidePerMWHe(double megawattHours) {
-		double ratio = this.gramsCarbonDioxidePerKWHe / 1000;
-		return ratio * megawattHours;
+
+	public double getKilogramsCarbonDioxideForKWHeValue(double kilowattHours) {
+		return (this.getKilogramsCarbonDioxidePerKWHe() * kilowattHours);
+	}
+
+	public double getKilogramsCarbonDioxidePerMWHe() {
+		return this.gramsCarbonDioxidePerKWHe;
+	}
+
+	public double getKilogramsCarbonDioxideForMWHeValue(double megawattHours) {
+		return (this.getKilogramsCarbonDioxidePerMWHe() * megawattHours);
+	}
+
+	public double getMetricTonnesCarbonDioxidePerMWHe() {
+		return (this.getKilogramsCarbonDioxidePerMWHe() / 1000);
+	}
+
+	public double getMetricTonnesCarbonDioxideForMWHeValue(double megawattHours) {
+		return (this.getMetricTonnesCarbonDioxidePerMWHe() * megawattHours);
 	}
 }
